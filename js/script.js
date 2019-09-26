aafce42339d538b1aa390e387245a9ee075037c2
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-09-26 16:04:43 +0200

    add 2 const for btn1 and btn2 + test the eventlistener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 "use strict";
 
+const buttonPrevious = document.querySelector(".btn1");
+const buttonNext = document.querySelector(".btn2");
 
+const li = document.querySelector("li");
+
+
+
+
+
+buttonPrevious.addEventListener("click", () => alert("ok"));
+buttonNext.addEventListener("click", () => alert("ok"));

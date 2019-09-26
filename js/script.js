738a61b9805191d38b10ddacb13c66ea59519249
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-09-26 16:37:26 +0200

    add function nextImage

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@
 
 const buttonPrevious = document.querySelector(".btn1");
 const buttonNext = document.querySelector(".btn2");
-
 const li = document.querySelector("li");
+let positionNext = 0;
+
+function nextImage(){
+    positionNext++;
+    li.classList.remove("active");
+}
 
 
 
